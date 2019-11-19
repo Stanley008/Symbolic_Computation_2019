add1d499f93bc70cd9ba4a4599f30c5612828c8b
Tocrak <virgiliustsirdea@yandex.ru>
2019-11-19 11:43:42 +0100

    remove unnecessary data from core.clj

--- src/park_chatbot/core.clj
+++ src/park_chatbot/core.clj
@@ -1,25 +1,6 @@
 (ns park-chatbot.core
     (:require [opennlp.nlp :as nlp]))
 
-"""define Park record """
-(defrecord Park [name wc restaurant cafe bicycle_paths skating sports_ground playground dogs opening_hours])
-
-"""create a defrecord variable Park"""
-(def betramka (->Park "Betramka" true false true true false false false nil "All year round with different closing hours"))
-(def fran_garden (->Park "Františkánská zahrada" true false false true false false true true "All year round but closes at night"))
-(def obora (->Park "Obora Hvězda" true true true true false false true true "All year round"))
-(def kampa (->Park "Kampa" true true true true true false true nil "All year round"))
-(def zahrada (->Park "Kinského zahrada" true false true false false false true nil "All year round"))
-(def klam (->Park "Klamovka" true true false true false true true true "All year round"))
-(def ladronka (->Park "Ladronka" true true false true true false true true "All year round"))
-(def letna (->Park "Letná" true true false true true true true true "All year round"))
-(def petrin (->Park "Petřín" true true false true true false true nil "All year round"))
-(def riegrovy (->Park "Riegrovy sady" true true false true true true true true "All year round"))
-(def stromovka (->Park "Stromovka" true true false true true true true true "All year round"))
-(def vajanovy (->Park "Vojanovy sady" false false false false false false false true "All year round"))
-(def vysehrad (->Park "Vyšehrad" true true true true false true true nil "All year round"))
-
-
 (def tokenize (nlp/make-tokenizer "src/en-token.bin"))
 (def name-find (nlp/make-name-finder "src/en-ner-person.bin"))
 
