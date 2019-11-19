752c4cfb2f47d3b4a3d78db69cc18f79cc146d71
Stanley008 <31549005+Stanley008@users.noreply.github.com>
2019-11-19 11:54:50 +0100

    Update main function
    Update, create a list of greetings .  Use it in -main function(to be code more readable).

--- src/park_chatbot/core.clj
+++ src/park_chatbot/core.clj
@@ -1,5 +1,6 @@
 (ns park-chatbot.core
-    (:require [opennlp.nlp :as nlp]))
+    (:require [opennlp.nlp :as nlp]
+			  [park-chatbot.data :as data]))
 
 (def tokenize (nlp/make-tokenizer "src/en-token.bin"))
 (def name-find (nlp/make-name-finder "src/en-ner-person.bin"))
@@ -8,5 +9,7 @@
   (let [user_input (read-line)]
     user_input))
 
-(defn main []
-  (rand-nth ["Hello, I am Parkbot. I am the most advanced park information bot and I am here to help you. Hello, I am Parkbot. " "I am here to serve your needs." "Hi, I am Parkbot. I am here today to help you to find the best park just for you." "Welcome, I am Parkbot. I am here to help you to find the best park just for you." "Hi, I am Parkbot. It’s a pleasure to meet you. I am here to help you." "Hey there, I am Parkbot. I am advanced interficial park information bot and I am here to help you to find the most suitable park just for you." "Good day. I am the most advanced interfacial park information bot. My name is Parkbot." "Howdy, my name is Parkbot. I am here to help you." "Great to see you. My name is Parkbot. I am advanced park information bot and I am here to help you." "Nice to see you. I am a Parkbot living in Prague. I would like to help you to find the right park just for you." "Look who it is! It’s me Parkbot and I am here today to help you."]))
+(defn -main []
+  (dosync
+    (println (rand-nth data/greetings))))
+
