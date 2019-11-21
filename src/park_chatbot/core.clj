7e12c56e00948641138ba8ec8098316c9d227f7f
Tocrak <virgiliustsirdea@yandex.ru>
2019-11-21 11:36:53 +0100

    create function find_name and ask_for_nickname in order to get the user's name/nickname

---
+++ src/park_chatbot/core.clj
@@ -0,0 +1,47 @@
+(ns park-chatbot.core
+    (:require [opennlp.nlp :as nlp]
+        [park-chatbot.data :as data]
+        [clojure.string :as str]))
+
+(def tokenize (nlp/make-tokenizer "src/en-token.bin"))
+
+(defn take_user_input []
+  """Take user input from CLI"""
+  (let [user_input (read-line)]
+    user_input))
+
+(defn strip_punctuation [text]
+  """Remove puntuation from a string"""
+  (str/replace text #"[.?,;:!]" ""))
+
+(defn find_name [sent]
+  (with-local-vars [name nil]
+    (doseq [token sent]
+      (if (= token (str/capitalize token))
+        (var-set name token)
+        (if (= @name nil)
+          (var-set name "Visitor"))))
+    @name))
+
+(defn ask_for_nickname []
+  (dosync
+    (println "Do you want me to call you by a nickname?")
+    (let [answer (take_user_input)]
+      (if (not (nil? (re-find #"[yY]es" answer)))
+        (do
+          (println "What nickname would you like?")
+          (ref-set (:name data/user)
+            (find_name (tokenize (strip_punctuation (take_user_input)))))
+          (println "Then I will call you" @(:name data/user)))
+        (println "As you wish master.")))))
+
+(defn -main []
+  """Main function"""
+  (dosync
+    (print (rand-nth data/greetings))
+    (newline)
+    (println (rand-nth data/name_ask))
+    (ref-set (:name data/user)
+      (find_name (tokenize (strip_punctuation (take_user_input)))))
+    (println "Welcome" @(:name data/user))
+    (ask_for_nickname)))
