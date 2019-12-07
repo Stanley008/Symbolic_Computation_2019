a5315b358b8ebb56d5296236e845c16801eb81c6
Tocrak <virgiliustsirdea@yandex.ru>
2019-12-07 12:27:45 +0100

    update project.clj, remove unnecessary dependencies

--- project.clj
+++ project.clj
@@ -1,11 +1,9 @@
 (defproject park_chatbot "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Park_chatbot with Clojure"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [clojure-opennlp "0.5.0"]
-                 [org.clojars.cognesence/matcher "1.0.1"]
-                 [org.clojure/core.logic "0.8.11"]]
+                 [clojure-opennlp "0.5.0"]]
   :repl-options {:init-ns park-chatbot.core}
   :main park-chatbot.core)
