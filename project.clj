e8b7ddc033b717797370f6b4bb171781c3a1cb99
Tocrak <virgiliustsirdea@yandex.ru>
2020-01-11 20:08:58 +0100

    Add the pre-trained neural network file, update dependencies

--- project.clj
+++ project.clj
@@ -4,6 +4,9 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [clojure-opennlp "0.5.0"]]
+                 [clojure-opennlp "0.5.0"]
+                 [thinktopic/cortex "0.9.22"]
+                 [net.mikera/imagez "0.12.0"]
+                 [thinktopic/think.image "0.4.16"]]
   :repl-options {:init-ns park-chatbot.core}
   :main park-chatbot.core)
