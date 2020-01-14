dcc4a01cdb02bd52a1efede50ea0fe1419356f9c
Tocrak <virgiliustsirdea@yandex.ru>
2020-01-14 13:00:20 +0100

    remove find_preference, match_park functions

--- src/park_chatbot/core.clj
+++ src/park_chatbot/core.clj
@@ -53,37 +53,6 @@
         (if (contains? data/neg_preference word)
           (println (rand-nth data/nickname_end)))))))
 
-(defn match_park
-  "Creates/Updates the list of selected parks based on the user preference.
-  topic -> the facility of the park that the function searches for;
-  user_preference -> boolean, represents wether the user want that facility included;
-  selected_parks -> reference to the list of parks with suitable preferences."
-  [topic user_preference selected_parks counter]
-  (if (= 1 counter)
-    (doseq [park data/parks]
-      (if (= user_preference (topic park))
-        (var-set selected_parks (conj @selected_parks park))))
-    (doseq [park @selected_parks]
-      (if (not= user_preference (topic park))
-        (var-set selected_parks (remove #{park} @selected_parks))))))
-
-(defn find_preferences
-  "Finds user's preference about a park facility and updates the facility preference
-  values in the user record with true/false. Then it calls 'match_park' function.
-  question_obj -> the question object (see 'question_objects' from data.clj);
-  selected_parks -> reference to the list of parks with suitable preferences."
-  [question_obj answer selected_parks counter]
-  (let [tokens (tokenize (str/lower-case answer))]
-    (doseq [word tokens]
-      (if (contains? data/pos_preference word)
-        (do
-          (ref-set ((:topic question_obj) data/user) true)
-          (match_park (:topic question_obj) true selected_parks counter))
-        (if (contains? data/neg_preference word)
-          (do
-            (ref-set ((:topic question_obj) data/user) false)
-            (match_park (:topic question_obj) false selected_parks counter)))))))
-
 (defn approve_ending?
   "Confirms if the user really want to end the conversation with the cahtbot.
   If yes updates the 'terminate' value in 'user' record to true.
